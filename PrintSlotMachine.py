916f7b393c65b1142af1ac45ceb2530d8f502541
Potoro <114840536+Potoro-Pao@users.noreply.github.com>
2023-05-30 22:44:16 +0800

    Update PrintSlotMachine.py

--- PrintSlotMachine.py
+++ PrintSlotMachine.py
@@ -1,18 +1,18 @@
 import random
 
 symbol_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-random_symbol_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# random_symbol_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 reel = [[random.choice(symbol_list) for _ in range(3)] for _ in range(3)]
 transpose_reel = [[reel[j][i] for j in range(3)] for i in range(3)]
-# 1 0
-# 2
-# 3
-# 4
-# 5
-print("original data", reel)
+                        # 1 0
+                        # 2
+                        # 3
+                        # 4
+                        # 5
+# print("original data", reel)
 
-print(reel)
+# print(reel)
 print(transpose_reel)
 
 for i in range(3):
@@ -24,10 +24,10 @@ for i in range(3):
 
     print()
 
-item_dict = {}
-for i, reel in enumerate(transpose_reel):
-    for j, item in enumerate(reel):
-        item_dict["key" + str(i) + str(j)] = item
+# item_dict = {}
+# for i, reel in enumerate(transpose_reel):
+#     for j, item in enumerate(reel):
+#         item_dict["key" + str(i) + str(j)] = item
 
 # print(item_dict)
 if transpose_reel[0][0] == transpose_reel[0][1] == transpose_reel[0][2]:
